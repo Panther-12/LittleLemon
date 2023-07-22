a105752fb4958cde9a5396c90041aad8c977c870
Panther-12 <nimrodwalwe@gmail.com>
2023-07-22 06:05:25 +0300

    Specify command to run the project

--- LittleLemon/littlelemon/settings.py
+++ LittleLemon/littlelemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
